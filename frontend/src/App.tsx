e92903951af77d6b4d2434c25780bee7f46d6f95
Sebastien Boisvert <sebhtml@protonmail.com>
2022-02-23 17:01:24 -0500

    frontend(dev) add Directory and Directories components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface DirectoryProps {
+  absolutePath: string;
+  files: string[];
+}
+
+function Directory(props: DirectoryProps) {
+  const listItems = props.files.map((filename) =>
+    <li>{filename}</li>
+    );
+
+  return <div><h1>{props.absolutePath}</h1><ul>{listItems}</ul></div>;
+}
+
+interface DirectoriesProps {
+  directories: string[]
+};
+
+function Directories(props: DirectoriesProps) {
+  //return <img />;
+  const elements = props.directories.map((directory) => {
+      return <Directory absolutePath={directory} files={["a.txt", "b.txt", "c.txt"]} />
+      });
+  return <div>{elements}</div>;
+  /*
+  */
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Directories directories={["/boot", "/proc", "/home"]} />;
 }
 
 export default App;
