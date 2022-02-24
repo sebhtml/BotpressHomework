a9417b503b0e039d1bb157f9a03b901caf36d4ff
Sebastien Boisvert <sebhtml@protonmail.com>
2022-02-24 12:25:06 -0500

    devops(dev): improve developer experience for local deployments

---
+++ deploy-locally.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if $(docker ps | grep sebhtml | wc -l ) -gt 1
+then
+  docker kill $(docker ps | grep "sebhtml/file-explorer"|awk '{print $1}')
+fi
+
+# backend
+cd backend
+docker build . -t sebhtml/file-explorer-backend:v1
+cd ..
+
+# frontend
+cd frontend
+docker build . -t sebhtml/file-explorer-frontend:v1
+cd ..
+
+docker run -d -p 64000:4000 sebhtml/file-explorer-backend:v1
+docker run -d -p 63000:3000 sebhtml/file-explorer-frontend:v1
+
